0f48a30f4941566505bad30474ebd7259be3bbce
Alex Burns <jharopa@gmail.com>
2022-06-27 14:14:06 +0100

    Added react-icons
    Added react-icons as a dependecy and swapped out exsisting icons in Header and Foooter components for react-icons versions.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import NavLink from './NavLink';
 import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
+import { BsSun, BsMoon } from 'react-icons/bs';
 
 const Header = () => {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -10,41 +11,15 @@ const Header = () => {
 
     if (currentTheme == 'dark') {
       return (
-        <button className="mr-5" onClick={() => setTheme('light')}>
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-            ></path>
-          </svg>
+        <button className="mr-8" onClick={() => setTheme('light')}>
+          <BsSun size={20} />
         </button>
       );
     }
 
     return (
-      <button className="mr-5" onClick={() => setTheme('dark')}>
-        <svg
-          className="h-6 w-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          ></path>
-        </svg>
+      <button className="mr-8" onClick={() => setTheme('dark')}>
+        <BsMoon size={20} />
       </button>
     );
   };
