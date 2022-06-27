f8caa0320a2b1c86b66d3b38acb9bfc0c6bc4f49
Alex Burns <jharopa@gmail.com>
2022-06-27 13:51:59 +0100

    Added Footer Animation
    Used Framer Motion to animate the Footer component when it comes into the viewport.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   const [commits, setCommits] = useState([]);
@@ -10,7 +11,17 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className="my-6 flex h-16 items-center justify-center text-slate-500">
+    <motion.footer
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false }}
+      transition={{ duration: 1.0 }}
+      variants={{
+        hidden: { y: 25, opacity: 0 },
+        visible: { y: 0, opacity: 1 },
+      }}
+      className="my-6 flex h-16 items-center justify-center text-slate-500"
+    >
       <a
         href="https://github.com/Jharopa/personal-site"
         className="hover:text-black dark:hover:text-white"
@@ -31,7 +42,7 @@ const Footer = () => {
           <span className="px-2 pb-0.5">{commits}</span>
         </div>
       </a>
-    </footer>
+    </motion.footer>
   );
 };
 
