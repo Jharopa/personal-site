424eaff1c0642680d82f83c4fe918d835570b120
Alex Burns <jharopa@gmail.com>
2022-06-28 11:03:18 +0100

    Fixed Bug on MacOS
    Updated Hero component's animation values to fix visual bug with animation on MacOS.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <Section>
       <motion.div
-        initial={{ y: -200, opacity: 0 }}
+        initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1.5 }}
         className="flex h-2/3 flex-col justify-center text-center"
@@ -13,7 +13,7 @@ const Hero = () => {
         <p className="pb-3">Hey there! I&apos;m</p>
         <h1 className="pb-16 text-6xl">Alexander Burns</h1>
         <motion.p
-          initial={{ x: -500, opacity: 0 }}
+          initial={{ x: -250, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.5, delay: 2.0 }}
           className="pb-3 text-center"
@@ -21,7 +21,7 @@ const Hero = () => {
           What I like,
         </motion.p>
         <motion.h1
-          initial={{ x: 500, opacity: 0 }}
+          initial={{ x: 250, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.5, delay: 2.0 }}
           className="pb-3 text-center text-5xl"
@@ -29,7 +29,7 @@ const Hero = () => {
           3D Graphics Programming.
         </motion.h1>
         <motion.h1
-          initial={{ x: -500, opacity: 0 }}
+          initial={{ x: -250, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1.5, delay: 2.0 }}
           className="text-center text-5xl"
