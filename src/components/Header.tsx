bfd76086cccb4ed080646e3079821c3ee7f3c750
Alex Burns <jharopa@gmail.com>
2022-07-03 20:21:58 +0100

    Removed Animations
    Removed animations from header and hero for now as they were stuttering during model loading.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,10 @@
-import { motion } from 'framer-motion';
-
 import Nav from './Nav';
 
 const Header = () => {
   return (
-    <motion.header
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.0, delay: 4.5 }}
-      className="fixed z-[100] flex h-16 w-screen items-center justify-center bg-white dark:bg-black"
-    >
+    <header className="fixed z-[100] flex h-16 w-screen items-center justify-center bg-white dark:bg-black">
       <Nav />
-    </motion.header>
+    </header>
   );
 };
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,45 +1,19 @@
-import { motion } from 'framer-motion';
-
 import Section from './Section';
 
 const Hero = () => {
   return (
     <Section>
-      <motion.div
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5, duration: 1.5 }}
-        className="m-auto flex h-full flex-col items-center justify-center text-center"
-      >
+      <div className="m-auto flex h-full flex-col items-center justify-center text-center">
         <p className="pb-3 text-lg">Hey there! I&apos;m</p>
         <h1 className="animate-text bg-gradient-to-r from-teal-500 via-indigo-500 to-pink-500 bg-clip-text pb-16 text-5xl text-transparent md:text-7xl">
           Alexander Burns
         </h1>
-        <motion.p
-          initial={{ x: -250, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.5, delay: 3.0 }}
-          className="pb-3 text-center text-lg"
-        >
-          What I like,
-        </motion.p>
-        <motion.h1
-          initial={{ x: 250, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.5, delay: 3.0 }}
-          className="pb-3 text-center text-3xl md:text-5xl"
-        >
+        <p className="pb-3 text-center text-lg">What I like,</p>
+        <h1 className="pb-3 text-center text-3xl md:text-5xl">
           3D Graphics Programming.
-        </motion.h1>
-        <motion.h1
-          initial={{ x: -250, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1.5, delay: 3.0 }}
-          className="text-center text-3xl md:text-5xl"
-        >
-          Web Development.
-        </motion.h1>
-      </motion.div>
+        </h1>
+        <h1 className="text-center text-3xl md:text-5xl">Web Development.</h1>
+      </div>
     </Section>
   );
 };
